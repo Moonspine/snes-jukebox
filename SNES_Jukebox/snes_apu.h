e6b75d7a30e1fe93bfc6cdf2e0e7b83f102477b0
Michael Calvin <pika242@gmail.com>
2020-03-23 18:24:43 -0500

    Cleanup

--- SNES_Jukebox/snes_apu.h
+++ SNES_Jukebox/snes_apu.h
@@ -6,32 +6,45 @@
 unsigned char readdata(unsigned char address) {
   unsigned char data;
 
+  // Set data pins as inputs
   PORT_DIR_APU_D0_1_CTRL = 0x0F;
   PORT_DIR_APU_D2_7 &= ~0xFC;
-  PORT_OUT_APU_D0_1_CTRL &= ~0x03;
-  PORT_OUT_APU_D0_1_CTRL |= (address & 0x03);
 
-  PORT_OUT_APU_D0_1_CTRL &= ~0x04;
+  // Set bits 0, 1 to address
+  // Ser /RD low (active)
+  PORT_OUT_APU_D0_1_CTRL = ((PORT_OUT_APU_D0_1_CTRL & ~0x03) | (address & 0x03)) & ~0x04;
+
+  // Read delay
   __asm__ __volatile__ ("nop");
   __asm__ __volatile__ ("nop");
+
+  // Read data
   data = ((PORT_IN_APU_D0_1_CTRL & 0x30) >> 4) | (PORT_IN_APU_D2_7 & 0xFC);
+
+  // Set /RD high (inactive)
   PORT_OUT_APU_D0_1_CTRL |= 0x04;
+  
   return data;
 }
 
 void writedata(unsigned char address, unsigned char data) {
+  // Set data pins as outputs
   PORT_DIR_APU_D0_1_CTRL = 0x3F;
   PORT_DIR_APU_D2_7 |= 0xFC;
 
-  PORT_OUT_APU_D2_7 &= ~0xFC;
-  PORT_OUT_APU_D2_7 |= (data & 0xFC);
+  // Set bits 2..7 to data bits 2..7
+  PORT_OUT_APU_D2_7 = (PORT_OUT_APU_D2_7 & ~0xFC) | (data & 0xFC);
 
-  PORT_OUT_APU_D0_1_CTRL &= ~0x33;
-  PORT_OUT_APU_D0_1_CTRL |= (address & 0x03);
-  PORT_OUT_APU_D0_1_CTRL |= ((data & 0x03) << 4);
-  PORT_OUT_APU_D0_1_CTRL &= ~0x08;
+  // Clear address an data bits and set /WR low (active)
+  // Set bits 0, 1 to address
+  // Set bits 4, 5 to data bits 0, 1
+  PORT_OUT_APU_D0_1_CTRL = (PORT_OUT_APU_D0_1_CTRL & ~0x3B) | (address & 0x03) | ((data & 0x03) << 4);
+
+  // Write delay
   __asm__ __volatile__ ("nop");
   __asm__ __volatile__ ("nop");
+
+  // Set /WR high (inactive)
   PORT_OUT_APU_D0_1_CTRL |= 0x08;
 }
 
@@ -45,4 +58,3 @@ void resetApu() {
 }
 
 #endif
-
--- makeLoaderHeader.bat
+++ makeLoaderHeader.bat
@@ -1,2 +1,2 @@
-TPS/bass -arch=snes-smp-canonical -o Song_Loader/song_loader.bin Song_Loader/song_loader.asm
-TPS/Bin2Header Song_Loader/song_loader.bin > Song_Loader/song_loader.h
+TPS\bass -arch=snes-smp-canonical -o Song_Loader\song_loader.bin Song_Loader\song_loader.asm
+TPS\Bin2Header Song_Loader\song_loader.bin > Song_Loader\song_loader.h
